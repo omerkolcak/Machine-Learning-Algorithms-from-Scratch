07a2b3b79a1e94beca2dd8e919a6ec9e1769586a
omerkolcak <o.faruk.kolcak@gmail.com>
2022-10-02 18:29:09 +0300

    add regularization to logistic regression

--- Classification/1 - Logistic Regression/LogisticRegressor.py
+++ Classification/1 - Logistic Regression/LogisticRegressor.py
@@ -1,21 +1,19 @@
 import numpy as np
 import random
 
-def log_loss(probs,y):
-    return np.sum(-y*np.log(probs + 0.00001) - (1-y)*np.log(1+0.00001-probs)) / y.shape[0]
-
 def sigmoid_function(theta):
     probs = 1 / (1 + np.exp(-theta))
     return probs
 
 class LogisticRegressor():
 
-    def __init__(self, learning_rate=0.01, iterations=100):
+    def __init__(self, learning_rate=0.01, iterations=100,reg_lambda=0):
         self.w = None
         self.b = None
         self.m = None
         self.learning_rate = learning_rate
         self.iterations = iterations
+        self.reg_lambda = reg_lambda
     
     def fit(self,x,y):
         self.m = x.shape[0]
@@ -24,20 +22,23 @@ class LogisticRegressor():
         self.b = random.uniform(0, 1)
 
         for iter in range(self.iterations):
-            yhat = self.predict(x)
-
-            d_w = np.dot(x.T,yhat - y) / self.m # (n,m) * (m,1) -> (n,1)
-            d_b = np.sum(yhat - y) / self.m
-
-            self.w -= self.learning_rate * d_w
-            self.b -= self.learning_rate * d_b
 
             if iter % 50 == 0:
                 theta = np.dot(x,self.w) + self.b # (m,n) * (n,1) -> (m,1)
                 probs = sigmoid_function(theta)
 
-                loss = log_loss(probs,y)
+                loss = self.log_loss(probs,y,self.reg_lambda)
                 print(f"{iter}. Iteration Training loss: {loss}")
+                print(self.w,self.b)
+
+            yhat = self.predict(x)
+
+            d_w = np.dot(x.T,yhat - y) / self.m - ((self.reg_lambda * self.w) / self.m) # (n,m) * (m,1) -> (n,1)
+            d_b = np.sum(yhat - y) / self.m
+
+            self.w -= self.learning_rate * d_w
+            self.b -= self.learning_rate * d_b
+
 
 
     def predict(self,x):
@@ -45,3 +46,6 @@ class LogisticRegressor():
         probs = 1 / (1 + np.exp(-theta))
 
         return [1 if p >= 0.5 else 0 for p in probs]
+
+    def log_loss(self,probs,y,reg_lambda=0):
+        return np.sum(-y*np.log(probs + 0.00001) - (1-y)*np.log(1+0.00001-probs)) / y.shape[0] + (reg_lambda * np.sum(np.square(self.w))) / (2 * self.m)
